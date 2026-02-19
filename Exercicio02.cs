92d323e6a3b1f9bbb0976d52003a97bf705cd727
joaomendes011 <joaovictormendes1827@gmail.com>
2026-02-19 11:33:54 -0300

    Entregando Ex1 , 2 e 3

---
+++ Exercicio02.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Joao Victor";
+    
+    public static void Executar()
+    {
+        string nome = "Joao Victor";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
